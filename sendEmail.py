3b441110f2b21c0f91b16214011956de52489a7f
ping <xiejianping@leomaster.com>
2022-07-15 11:46:37 +0800

    Changes

--- sendEmail.py
+++ sendEmail.py
@@ -1,6 +1,6 @@
 import yagmail
 
-# 发送邮件 MVICPWPKFCKPZIXE
+# 发送邮件
 if __name__ == '__main__':
     yag = yagmail.SMTP(user='user@163.com', password='nicai', host='smtp.163.com', port='465')
     yag.send('reciver@qq.com', subject="I now can send an attachment")
