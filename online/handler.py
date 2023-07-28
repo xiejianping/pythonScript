465b836fd9ac5cf3dd87ba45102560d326136e04
ping <ping@email.com>
2023-07-28 12:25:05 +0400

    提交

--- online/handler.py
+++ online/handler.py
@@ -34,3 +34,25 @@ def replaceManifest(oldpkg, newpkg):
 def replacepkg(oldpkg, newpkg):
     replaceManifest(oldpkg, newpkg)
     replaceSmali(oldpkg, newpkg)
+
+
+def replaceAll(oldpkg, newpkg):
+    oldSmalipkg = f"L{oldpkg.replace('.', '/')}"
+    newSmalipkg = f"L{newpkg.replace('.', '/')}"
+    print(f'{oldSmalipkg}   {newSmalipkg}')
+    listDir = os.listdir(output)
+
+    for root, dirs, files in os.walk(f'{output}'):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path)
+            with open(file_path, mode='r', encoding='utf-8') as f:
+                try:
+                    file_content = f.read()
+                except:
+                    continue
+            file_content = file_content.replace(oldSmalipkg, newSmalipkg).replace(oldpkg, newpkg)
+            with open(file_path, mode='w', encoding='utf-8') as f:
+                f.write(file_content)
+
+
--- online/main.py
+++ online/main.py
@@ -1,11 +1,12 @@
 from online.decode import decodeOnline
 from online.encode import compileOnline
-from online.handler import replacepkg
+from online.handler import replacepkg, replaceAll
 
 
 def start():
     # decodeOnline()
-    replacepkg("com.unity3d.player.UnityPlayerActivity","com.dqbf.iaf.xrfm.JSDdjs")
+    replacepkg("com.unity3d.player.UnityPlayerActivity", "org.cocos2dx.javascript.AppActivity")
+    replaceAll("net.kosev.dicing", "aab.as.xsa")
     compileOnline()
 
 
